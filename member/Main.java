e06f5e15ef4d45d56177ee875ebb585fa8131a0f
hirokiyamajo <124019567+hirokiyamajo@users.noreply.github.com>
2024-09-15 23:08:02 +0900

    member 課題(1)
    確認よろしくお願いします。

--- member/Main.java
+++ member/Main.java
@@ -4,6 +4,8 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
+import member.bean.AdminUser;
+import member.bean.GeneralUser;
 import member.bean.User;
 
 public class Main {
@@ -45,20 +47,53 @@ public class Main {
 
     // 一般ユーザーを登録するメソッド
     public static void registerGeneralUser(Scanner scanner) {
+        //一般ユーザーの情報(名前、メールアドレス)を取得する
+        System.out.println("名前を入力してください：");
+        String userName = scanner.next();
+        System.out.println("メールアドレスを入力してください：");
+        String email = scanner.next();
         
+        //取得した情報をもとに一般ユーザーを作成する
+        User newUser = new GeneralUser(userName, email);
+        
+        //一般ユーザーをリスト(保管場所)に格納する
+        users.add(newUser);
+        
+        System.out.println("一般ユーザーの登録をしました");
     }
 
     // 管理者ユーザーを登録するメソッド
     public static void registerAdminUser(Scanner scanner) {
+        System.out.println("名前を入力してください：");
+        String adminName = scanner.next();
+        System.out.println("メールアドレスを入力してください：");
+        String email = scanner.next();
+        System.out.println("管理者コードを入力してください：");
+        String adminCode = scanner.next();
+        
+        User newUser1 = new AdminUser(adminName, email,adminCode);
+        
+        users.add(newUser1);
         
     }
     // メールアドレスが既に登録されているか確認するメソッド
     public static boolean isEmailRegistered() {
+        
+        //登録されているメールアドレスを取得
+        users.getEmail();
+        //取得されたメールアドレスが登録されているかを確認する
+        
+        
         return false;
         
     }
     // 登録されたユーザーを表示するメソッド
     public static void findUsers() {
         
+        
+       for (User user : users) {
+            System.out.println(users);
+       } 
+       
     }
 }
